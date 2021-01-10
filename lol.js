258dedd716d1cf44f9b3926e10f681d2e0a57f3c
windsdeath <windsdeath@gmail.com>
2021-01-11 04:26:56 +0900

    out of memory...

---
+++ lol.js
@@ -0,0 +1,577 @@
+const text = `가렌
+        가렌 Gold 1
+        대장군 대장군
+        선봉대 선봉대
+        600 50 50 Attack Range 0.60 30 심판 MP 0 / 100
+        나서스
+        나서스 Gold 1
+        신성 신성
+        흡수자 흡수자
+        650 50 45 Attack Range 0.65 29 쇠약 MP 0 / 60
+        노틸러스
+        노틸러스 Gold 2
+        우화 우화
+        선봉대 선봉대
+        750 45 55 Attack Range 0.60 33 충격 분화구 MP 75 / 150
+        누누와 윌럼프
+        누누와 윌럼프 Gold 3
+        나무정령 나무정령
+        싸움꾼 싸움꾼
+        750 50 65 Attack Range 0.55 36 잡아먹기 MP 0 / 85
+        니달리
+        니달리 Gold 1
+        대장군 대장군
+        명사수 명사수
+        550 30 45 Attack Range 0.70 32 창 투척 MP 0 / 70
+        니코
+        니코 Gold 3
+        우화 우화
+        신비술사 신비술사
+        700 25 45 Attack Range 0.70 32 만개 MP 0 / 60
+        다리우스
+        다리우스 Gold 3
+        행운 행운
+        학살자 학살자
+        800 60 70 Attack Range 0.70 49 행운의 단두대 MP 0 / 50
+        다이애나
+        다이애나 Gold 1
+        영혼 영혼
+        암살자 암살자
+        550 30 50 Attack Range 0.70 35 은빛 가호 MP 0 / 70
+        라칸
+        라칸 Gold 2
+        나무정령 나무정령
+        귀감 귀감
+        600 40 45 Attack Range 0.70 32 무장해제 MP 40 / 90
+        룰루
+        룰루 Gold 2
+        나무정령 나무정령
+        요술사 요술사
+        500 20 50 Attack Range 0.60 30 급성장 MP 60 / 120
+        리신
+        리신 Gold 5
+        신성 신성
+        결투가 결투가
+        1000 50 60 Attack Range 0.85 51 신의 권 MP 0 / 40
+        마오카이
+        마오카이 Gold 1
+        나무정령 나무정령
+        싸움꾼 싸움꾼
+        700 40 55 Attack Range 0.50 28 덤불 주먹 MP 0 / 75
+        모르가나
+        모르가나 Gold 4
+        선지자 선지자
+        흡수자 흡수자
+        750 30 50 Attack Range 0.70 35 신성한 땅 MP 60 / 120
+        바이
+        바이 Gold 2
+        대장군 대장군
+        싸움꾼 싸움꾼
+        700 40 65 Attack Range 0.60 39 찌그러뜨리기 MP 0 / 50
+        베이가
+        베이가 Gold 3
+        나무정령 나무정령
+        요술사 요술사
+        600 20 40 Attack Range 0.65 26 어둠의 꽃 MP 30 / 70
+        브라움
+        브라움 Gold 2
+        용의 영혼 용의 영혼
+        선봉대 선봉대
+        800 60 40 Attack Range 0.60 24 불굴 MP 30 / 70
+        브랜드
+        브랜드 Gold 1
+        용의 영혼 용의 영혼
+        요술사 요술사
+        500 20 40 Attack Range 0.65 26 용의 불기둥 MP 0 / 60
+        블라디미르
+        블라디미르 Gold 2
+        광신도 광신도
+        흡수자 흡수자
+        750 30 40 Attack Range 0.60 24 수혈 MP 0 / 60
+        사미라
+        사미라 Gold 5
+        무모한 자 무모한 자
+        명사수 명사수
+        학살자 학살자 
+        900 30 65 Attack Range 0.85 55 지옥불 난사 -
+        세주아니
+        세주아니 Gold 4
+        행운 행운
+        선봉대 선봉대
+        1000 60 45 Attack Range 0.55 25 불꽃놀이 MP 70 / 150
+        세트
+        세트 Gold 5
+        우두머리 우두머리
+        싸움꾼 싸움꾼
+        1100 60 60 Attack Range 0.75 45 대미 장식 MP 100 / 175
+        쉔
+        쉔 Gold 4
+        닌자 닌자
+        신비술사 신비술사 조율자 조율자
+        950 60 60 Attack Range 0.65 39 그림자 돌진 MP 50 / 100
+        쉬바나
+        쉬바나 Gold 3
+        용의 영혼 용의 영혼
+        싸움꾼 싸움꾼
+        650 35 70 Attack Range 0.75 53 용의 강림 MP 0 / 75
+        스웨인
+        스웨인 Gold 5
+        용의 영혼 용의 영혼
+        흡수자 흡수자
+        1000 60 60 Attack Range 0.65 39 용의 승천 MP 40 / 100
+        시비르
+        시비르 Gold 3
+        광신도 광신도
+        명사수 명사수
+        650 20 70 Attack Range 0.85 60 사냥 개시 MP 30 / 80
+        아우렐리온 솔
+        아우렐리온 솔 Gold 4
+        용의 영혼 용의 영혼
+        요술사 요술사
+        650 20 40 Attack Range 0.75 30 번개의 공명 MP 0 / 80
+        아지르
+        아지르 Gold 5
+        대장군 대장군
+        귀감 귀감
+        황제 황제
+        950 30 70 Attack Range 0.85 60 의 진영 MP 75 / 150
+        아칼리
+        아칼리 Gold 3
+        닌자 닌자
+        암살자 암살자
+        650 20 55 Attack Range 0.85 47 오연투척검 MP 0 / 20
+        아트록스
+        아트록스 Gold 4
+        광신도 광신도
+        선봉대 선봉대
+        850 70 60 Attack Range 0.65 39 지옥사슬 MP 75 / 150
+        애니
+        애니 Gold 2
+        행운 행운
+        요술사 요술사
+        750 40 45 Attack Range 0.60 27 폭발 보호막 MP 0 / 65
+        야스오
+        야스오 Gold 1
+        추방자 추방자
+        결투가 결투가
+        550 30 50 Attack Range 0.70 35 강철의 일격 MP 0 / 45
+        엘리스
+        엘리스 Gold 1
+        광신도 광신도
+        귀감 귀감
+        600 30 45 Attack Range 0.65 29 거미 형태 MP 0 / 75
+        오공
+        오공 Gold 1
+        신성 신성
+        선봉대 선봉대
+        650 40 55 Attack Range 0.65 36 파쇄격 MP 0 / 50
+        오른
+        오른 Gold 5
+        나무정령 나무정령
+        대장장이 대장장이
+        선봉대 선봉대
+        900 50 100 Attack Range 0.55 55 쇄도 MP 80 / 160
+        올라프
+        올라프 Gold 4
+        용의 영혼 용의 영혼
+        학살자 학살자
+        900 50 85 Attack Range 0.80 68 라그나로크 MP 0 / 70
+        요네
+        요네 Gold 5
+        추방자 추방자
+        조율자 조율자
+        950 40 80 Attack Range 0.95 76 운명봉인 MP 0 / 60
+        유미
+        유미 Gold 3
+        영혼 영혼
+        신비술사 신비술사
+        650 20 40 Attack Range 0.60 24 슈우우웅 MP 0 / 40
+        이렐리아
+        이렐리아 Gold 3
+        선지자 선지자
+        신성 신성
+        조율자 조율자
+        800 40 55 Attack Range 0.65 36 칼날 폭풍 MP 50 / 100
+        자르반 4세
+        자르반 4세 Gold 2
+        대장군 대장군
+        귀감 귀감
+        600 45 60 Attack Range 0.60 36 용의 일격 MP 50 / 100
+        자야
+        자야 Gold 4
+        나무정령 나무정령
+        귀감 귀감
+        처형자 처형자
+        600 20 80 Attack Range 0.85 68 저항의 비상 MP 60 / 120
+        잔나
+        잔나 Gold 2
+        선지자 선지자
+        신비술사 신비술사
+        550 20 40 Attack Range 0.60 24 폭풍의 눈 MP 40 / 70
+        잭스
+        잭스 Gold 2
+        신성 신성
+        결투가 결투가
+        750 40 60 Attack Range 0.75 45 반격 MP 60 / 110
+        제드
+        제드 Gold 2
+        닌자 닌자
+        학살자 학살자
+        600 20 55 Attack Range 0.80 44 약자 멸시 -
+        질리언
+        질리언 Gold 5
+        광신도 광신도
+        신비술사 신비술사
+        750 20 45 Attack Range 0.75 34 운명의 역행 MP 100 / 150
+        초가스
+        초가스 Gold 4
+        우화 우화
+        싸움꾼 싸움꾼
+        1400 40 100 Attack Range 0.60 60 파열 MP 75 / 150
+        카타리나
+        카타리나 Gold 3
+        대장군 대장군
+        행운 행운
+        암살자 암살자
+        500 20 65 Attack Range 0.65 42 죽음의 연꽃 MP 0 / 90
+        칼리스타
+        칼리스타 Gold 3
+        광신도 광신도
+        결투가 결투가
+        650 20 65 Attack Range 0.90 59 뽑아 찢기 -
+        케넨
+        케넨 Gold 3
+        닌자 닌자
+        귀감 귀감
+        650 30 60 Attack Range 0.65 39 날카로운 소용돌이 MP 50 / 125
+        케일
+        케일 Gold 4
+        신성 신성
+        처형자 처형자
+        650 20 75 Attack Range 0.90 68 한 승천 MP 0 / 70
+        킨드레드
+        킨드레드 Gold 3
+        영혼 영혼
+        처형자 처형자
+        650 20 70 Attack Range 0.80 56 공포 세례 MP 0 / 35
+        탈론
+        탈론 Gold 4
+        선지자 선지자
+        암살자 암살자
+        800 30 90 Attack Range 0.80 72 예리한 일격 MP 0 / 40
+        탐 켄치
+        탐 켄치 Gold 1
+        행운 행운
+        싸움꾼 싸움꾼
+        700 40 85 Attack Range 0.50 43 두꺼운 피부 -
+        트리스타나
+        트리스타나 Gold 1
+        용의 영혼 용의 영혼
+        명사수 명사수
+        500 20 50 Attack Range 0.70 35 속사 MP 0 / 40
+        트린다미어
+        트린다미어 Gold 4
+        대장군 대장군
+        결투가 결투가
+        학살자 학살자
+        850 50 85 Attack Range 0.85 72 회전 베기 MP 30 / 80
+        트위스티드 페이트
+        트위스티드 페이트 Gold 1
+        광신도 광신도
+        요술사 요술사
+        550 20 45 Attack Range 0.65 29 와일드 카드 MP 0 / 70
+        티모
+        티모 Gold 2
+        영혼 영혼
+        명사수 명사수
+        500 20 55 Attack Range 0.70 39 포자 구름 다트 MP 0 / 60
+        파이크
+        파이크 Gold 2
+        광신도 광신도
+        암살자 암살자
+        학살자 학살자
+        700 50 50 Attack Range 0.60 30 망자의 물살 MP 60 / 120
+        피오라
+        피오라 Gold 1
+        선지자 선지자
+        결투가 결투가
+        600 35 45 Attack Range 0.75 34 응수 MP 0 / 75 `
+
+const parseText = text.split('\n').map(x => x.trim())
+
+function parseChamp() {
+    let copyText = [...parseText]
+    let copyTextL = copyText.length
+    let champs = [];
+    let champ = new Object();
+    champ.kind = []
+    champ.job = []
+    for (let i = 0; i < copyTextL; i++) {
+        if (/^[가-힣]+$|누누와 윌럼프|아우렐리온 솔|자르반 4세|탐 켄치|트위스티드 페이트/.test(copyText[0])) {
+            champ.name = copyText[0];
+            copyText.splice(0, 1);
+            champ.cost = copyText[0].match(/\d+/)[0];
+            copyText.splice(0, 1);
+            while (/광신도|나무정령|닌자|대장군|무모한 자|선지자|신성|용의 영혼|영혼|우두머리|우화|추방자|행운/.test(copyText[0])) {
+                champ.kind.push(copyText[0].match(/광신도|나무정령|닌자|대장군|무모한 자|선지자|신성|용의 영혼|영혼|우두머리|우화|추방자|행운/)[0]);
+                copyText.splice(0, 1);
+            }
+            while (/결투가|귀감|대장장이|명사수|선봉대|신비술사|싸움꾼|암살자|요술사|조율자|처형자|학살자|황제 |흡수자/.test(copyText[0])) {
+                champ.job.push(copyText[0].match(/결투가|귀감|대장장이|명사수|선봉대|신비술사|싸움꾼|암살자|요술사|조율자|처형자|학살자|황제|흡수자/)[0]);
+                copyText.splice(0, 1);
+            }
+            copyText.splice(0, 1);
+            champs.push(champ);
+            champ = new Object();
+            champ.kind = []
+            champ.job = []
+        }
+    }
+    return champs
+}
+const champs = parseChamp();
+
+function countValue(choosenChamp, choosen) { // 골라진 챔프와 선받자를 받아 현재 밸류를 리턴
+    let sumCostValue = choosenChamp.reduce(function(acc, cc) {
+        return Number(acc.cost) + Number(cc.cost)
+    })
+    let objChamps = makeSynergyObj(choosenChamp)
+    let sumSynergy = function(objChamps, choosen) {
+        objChamps[choosen] += 1;
+        currentValue = 0;
+
+        switch (objChamps) {
+            case objChamps["광신도"] >= 3:
+                currentValue += 4
+            case objChamps["광신도"] >= 6:
+                currentValue += 7
+            case objChamps["광신도"] >= 9:
+                currentValue += 11
+            case objChamps["나무정령"] >= 3:
+                currentValue += 4
+            case objChamps["나무정령"] >= 6:
+                currentValue += 9
+            case objChamps["나무정령"] >= 9:
+                currentValue += 12
+            case objChamps["닌자"] = 1:
+                currentValue += 2
+            case objChamps["닌자"] = 4:
+                currentValue += 10
+            case objChamps["대장군"] >= 3:
+                currentValue += 4
+            case objChamps["대장군"] >= 6:
+                currentValue += 10
+            case objChamps["대장군"] >= 9:
+                currentValue += 10
+            case objChamps["무모한 자"] >= 1:
+                currentValue += 4
+            case objChamps["선지자"] >= 2:
+                currentValue += 2
+            case objChamps["선지자"] >= 4:
+                currentValue += 6
+            case objChamps["선지자"] >= 6:
+                currentValue += 9
+            case objChamps["신성"] >= 2:
+                currentValue += 3
+            case objChamps["신성"] >= 4:
+                currentValue += 4
+            case objChamps["신성"] >= 6:
+                currentValue += 7
+            case objChamps["신성"] >= 8:
+                currentValue += 10
+            case objChamps["영혼"] >= 2:
+                currentValue += 4
+            case objChamps["영혼"] >= 4:
+                currentValue += 10
+            case objChamps["용의 영혼"] >= 3:
+                currentValue += 4
+            case objChamps["용의 영혼"] >= 6:
+                currentValue += 11
+            case objChamps["용의 영혼"] >= 9:
+                currentValue += 8
+            case objChamps["우두머리"] >= 1:
+                currentValue += 4
+            case objChamps["우화"] >= 3:
+                currentValue += 10
+            case objChamps["추방자"] >= 1:
+                currentValue += 1
+            case objChamps["추방자"] >= 2:
+                currentValue += 6
+            case objChamps["행운"] >= 3:
+                currentValue += 6
+            case objChamps["행운"] >= 6:
+                currentValue += 12
+            case objChamps["결투가"] >= 2:
+                currentValue += 3
+            case objChamps["결투가"] >= 4:
+                currentValue += 4
+            case objChamps["결투가"] >= 6:
+                currentValue += 8
+            case objChamps["결투가"] >= 8:
+                currentValue += 9
+            case objChamps["귀감"] >= 2:
+                currentValue += 4
+            case objChamps["귀감"] >= 4:
+                currentValue += 6
+            case objChamps["귀감"] >= 6:
+                currentValue += 9
+            case objChamps["대장장이"] = 1:
+                currentValue += 10
+            case objChamps["명사수"] >= 2:
+                currentValue += 4
+            case objChamps["명사수"] >= 4:
+                currentValue += 7
+            case objChamps["명사수"] >= 6:
+                currentValue += 10
+            case objChamps["선봉대"] >= 2:
+                currentValue += 3
+            case objChamps["선봉대"] >= 4:
+                currentValue += 5
+            case objChamps["선봉대"] >= 6:
+                currentValue += 7
+            case objChamps["선봉대"] >= 8:
+                currentValue += 9
+            case objChamps["신비술사"] >= 2:
+                currentValue += 4
+            case objChamps["신비술사"] >= 4:
+                currentValue += 7
+            case objChamps["신비술사"] >= 6:
+                currentValue += 9
+            case objChamps["싸움꾼"] >= 2:
+                currentValue += 3
+            case objChamps["싸움꾼"] >= 4:
+                currentValue += 5
+            case objChamps["싸움꾼"] >= 6:
+                currentValue += 6
+            case objChamps["싸움꾼"] >= 8:
+                currentValue += 9
+            case objChamps["암살자"] >= 2:
+                currentValue += 3
+            case objChamps["암살자"] >= 4:
+                currentValue += 6
+            case objChamps["암살자"] >= 6:
+                currentValue += 10
+            case objChamps["요술사"] >= 3:
+                currentValue += 4
+            case objChamps["요술사"] >= 5:
+                currentValue += 7
+            case objChamps["요술사"] >= 7:
+                currentValue += 11
+            case objChamps["조율자"] >= 2:
+                currentValue += 2
+            case objChamps["조율자"] >= 3:
+                currentValue += 4
+            case objChamps["조율자"] >= 4:
+                currentValue += 5
+            case objChamps["처형자"] >= 2:
+                currentValue += 2
+            case objChamps["처형자"] >= 3:
+                currentValue += 4
+            case objChamps["처형자"] >= 4:
+                currentValue += 5
+            case objChamps["학살자"] >= 3:
+                currentValue += 5
+            case objChamps["학살자"] >= 6:
+                currentValue += 12
+            case objChamps["황제"] >= 1:
+                currentValue += 5
+            case objChamps["흡수자"] >= 2:
+                currentValue += 4
+            case objChamps["흡수자"] >= 4:
+                currentValue += 10
+        }
+        return currentValue;
+    }
+    let sumSynergyValue = sumSynergy(objChamps, choosen)
+    let currentValue = sumCostValue + sumSynergyValue;
+    return currentValue;
+}
+
+
+function joinChamps(choosenChamp) {
+    return choosenChamp.reduce(function(arr, val) { // ["요술사","귀감","황제"]
+        return arr.concat(val.job.map((x, i) => x));
+    }, [])
+}
+
+function makeSynergyObj(choosenChamps) { // 챔프들의 배열을 받아 시너지값의 객체로 리턴함
+    return choosenChamps.reduce(function(SynergyObj, synergy) { // {요술사:1, 귀감:1, 황제:1}
+        if (synergy in SynergyObj) {
+            SynergyObj[synergy]++;
+        } else {
+            SynergyObj[synergy] = 1;
+        }
+        return SynergyObj;
+    }, {})
+}
+
+function choosenChamps() { // 챔프이름의 배열을 받아 챔프정보가 들어간 배열로 리턴
+    let copyChamps = champs
+    let choosenChamps = []
+    for (i in arguments[0]) {
+        let index = champs.findIndex(x => x.name == arguments[0][i]);
+        choosenChamps.push(copyChamps.splice(index, 1)[0])
+    }
+    return choosenChamps
+}
+
+function popChamp(openChamps, choosenC) {
+    for (c in choosenC) {
+        openChamps.pop(c);
+    }
+}
+
+const dfs = (openChamp, level, arr = []) => {
+    //3개를 선택한다는가정에 3개가 선택 됐다면 출력
+    if (level === level - openChamp.length) info.push([...arr]);
+    else {
+        for (let i = 0; i < openChamp.length; i++) {
+            arr.push(openChamp[i]);
+            dfs(openChamp.slice(i + 1), level + 1, arr);
+            arr.pop();
+        }
+    }
+};
+
+let info = [];
+
+function choice(choosenChamp, openChamps, level, choosen) { // 더 탐색할 챔프들의 배열을 받아 
+    let maxValue = 0;
+    if (level === 1) {
+        return {
+            "maxValue": choosenChamp["cost"] + sumSynergy(makeSynergyObj(choosenChamp)),
+            "Champs": choosenChamp["name"],
+            "choosen": choosen
+        }
+    } //레벨이 1이면 선택된 챔프의 정보로 바로 반환
+    let results = dfs(openChamps, level);
+
+    results.forEach(
+        function(x) {
+            let currentValue = countValue(x);
+            if (maxValue < currentValue) {
+                maxValue = currentValue;
+                info = [{ "maxValue": maxValue, "Champs": choosenChamp, "choosen": choosen }];
+            } else if (maxValue == currentValue) {
+                info.push({ "maxValue": maxValue, "Champs": choosenChamp, "choosen": choosen });
+            }
+        }
+    )
+    return info
+}
+
+function find(ChampNamesArray, level, choosen) { //챔프와 레벨을 받아서 최적의 조합 리턴
+    if (!Array.isArray(ChampNamesArray)) {
+        return console.log("챔프이름은 배열로만 입력해주세요.")
+    } else if (typeof level !== "number") {
+        return console.log("레벨은 숫자로만 입력해주세요.")
+    } else if (typeof choosen !== "string") {
+        return console.log("선받자는 문자열로만 입력해주세요.")
+    }
+    let choosenC = choosenChamps(ChampNamesArray); // ChampNamesArray을 받아 choosenC 배열에 챔프정보 저장
+    let openChamps = [...champs]; //챔프들 복사, 더 탐색해볼 챔프의 배열
+    popChamp(openChamps, choosenC); //openChams 배열에서 선택된 챔프를 뺌 
+    // choosenC 는 arg로 받은 챔프들을 가지고있고, openChamps는 arg로 받은 챔프 외의 챔프만 가지고 있는 상태
+    console.log(choice(choosenC, openChamps, level, choosen));
+}
+find(["다리우스", "노틸러스"], 3, "우화");
